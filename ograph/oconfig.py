1fcbdbd54694d45b16f917fb88ade598a9cd66c6
lyodine <commit@lyodine.com>
2025-04-14 13:33:00 -0400

    style(oconfig): Improve typing

--- ograph/oconfig.py
+++ ograph/oconfig.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib as mlp
 
-def axis_labels(enable=False):
+
+def axis_labels(enable: bool = False) -> None:
     plt.gca().get_yaxis().set_ticks([])
     plt.gca().get_xaxis().set_ticks([])
 
-def axis_lines(enable=True):
+
+def axis_lines(enable: bool = True) -> None:
     plt.gca().axhline(y=0, color='DimGrey', linestyle="--")
     plt.gca().axvline(x=0, color='DimGrey', linestyle="--")
 
-def large_text(enable=True):
+
+def large_text(enable: bool = True) -> None:
     mlp.rc("font", size=15)
